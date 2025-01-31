837549679a4a03b1dda4d9fb1c0e98c43b7ceeba
Syaniza <2022899418@student.uitm.edu.my>
2025-02-01 01:07:16 +0800

    First Commit

---
+++ donation.php
@@ -0,0 +1,46 @@
+<?php include './components/header.php'; ?>
+
+    <div class="ui container">
+
+        <!-- Top Navigation Bar -->
+        <?php include './components/top-menu.php'; ?>
+
+        <!-- BODY Content -->
+        <div class="ui grid">
+            <!-- Left menu -->
+            <?php include './components/side-menu.php'; ?>
+            
+            <!-- right content -->
+            <div class="twelve wide column">
+                <h1>Donations</h1>
+                <p><strong>Yayasan Kebajikan Anak-Anak Yatim</strong> relies on everyone's commitment to the humanitarian cause and it's underlying values. The strongest possible support from individuals, companies and foundations is essential if we are to meet the challenges we are currently facing. Your support to YKAY is more than just a donation - it is a true act of humanity.</p>
+                <p><strong>Let's join</strong> hands and support more and more needy children - the future of any nation and help them come out of their ignorance and other problems which it entails by giving them a gift of education and providing them with all-round support essential for their overall well-being. Your donations are a vital source of income that will allow us to make significant positive impact on the society around us.</p>
+
+                <p><strong>Cash donations</strong> can be made either personally or by sending an account payee Cheque / Draft through mail.</p>
+                <p>Info for making DD/Cheque: In favour of - <strong>Yayasan Kebajikan Anak-Anak Yatim</strong> </p>
+                <p>Yayasan Kebajikan Anak-Anak Yatim Taman Sabariah, Kota Bharu Kelantan- 154000. Ph: 09-7735494</p>
+                
+                <p><a class="fluid ui button" href="donation-form.php">Click here to make your contribution</a></p>
+                
+
+                <strong>Guideline for making contribution:</strong>
+                <ul>
+                    <li>RM100/- a month, can give a child Nutrition at learning centre.</li>
+                    <li>RM100/- a month, can give a child Nutrition & After-school support.</li>
+                    <li>RM200/- a month, can give a child complete package of Nutritio, health check-up, after-school, celebration of festivals etc.</li>
+                    <li>RM100/- a month, can be used for Sensitization & Awareness Programs including Media campaigns, workshops, seminars and capacity building initiatives.</li>
+                    <li>RM500/- a month, can be used for Infrastruture set up.</li>
+                    <li>RM100/- a month, can be used for Corpus Fund.</li>
+                </ul>
+
+                <p><strong>We respect the intention of our donors and use the money donated by them for the purpose intended by the donor.</strong></p>
+
+
+
+                <span class="p-20"></span>
+            </div>
+        </div>
+
+    </div>
+    
+<?php include './components/footer.php'; ?>
---
+++ photo-gallery.php
@@ -0,0 +1,82 @@
+<?php include './components/header.php'; ?>
+
+    <div class="ui container">
+
+        <!-- Top Navigation Bar -->
+        <?php include './components/top-menu.php'; ?>
+
+        <!-- BODY Content -->
+        <div class="ui grid">
+            <!-- Left menu -->
+            <?php include './components/side-menu.php'; ?>
+            
+            <!-- right content -->
+            <div class="twelve wide column">
+                <h1>Photo Gallery</h1>
+                <!-- <p><strong>CCDF</strong> is a non-profit, non-government and voluntary organization committed to the care & development and voluntary organization committed to the care and development of the underprivileged children.</p>
+                <p><strong>CCDF is a group</strong> of quanlified, hardworking, dedicated, like-minded people trying to make a difference in the life of the underrepresented, disadvantaged and marginalized sections of the society. It have been established to work as a platform to channelize and make optimum use of the resources and infrastructure available and people's desire to give back to society a bit of what they owe to it.</p>
+                <p><strong>It is out effort</strong> at CCDF to guide and motivate people to use their resources in a construction in changing the lives of these street children.</p> <p><strong>We are working</strong> in the field of education and over all development of the destitute children by providing then with an opportunity to realize their full potentials and lead a dignified and respectable life.</p> -->
+
+                <h2 class="ui block header">EVENTS </h2>
+                <div class="ui medium images">
+                    <img class="ui medium rounded image" src="img/EVENT1.jpg">
+                    <img class="ui medium rounded image" src="img/EVENT2.jpg">
+                    <img class="ui medium rounded image" src="img/EVENT3.jpg">
+                    <img class="ui medium rounded image" src="img/EVENT4.jpg">
+                </div>
+
+                <h2 class="ui block header">ACTIVITIES</h2>
+                <div class="ui medium images">
+                    <img class="ui medium rounded image" src="img/ACTIVITIES1.jpg">
+                    <img class="ui medium rounded image" src="img/ACTIVITIES2.jpg">
+                    <img class="ui medium rounded image" src="img/ACTIVITIES3.jpg">
+                    <img class="ui medium rounded image" src="img/ACTIVITIES4.jpg">
+                </div>
+
+                <h2 class="ui block header">INDEPENDENCE DAY</h2>
+                <div class="ui medium images">
+                    <img class="ui medium rounded image" src="img/DAY1.jpg">
+                    <img class="ui medium rounded image" src="img/DAY2.jpg">
+                    <img class="ui medium rounded image" src="img/DAY3.jpg">
+                    <img class="ui medium rounded image" src="img/DAY4.jpg">
+                </div>
+
+                <h2 class="ui block header">SPORTS DAY</h2>
+                <div class="ui medium images">
+                    <img class="ui medium rounded image" src="img/SPORT1.jpg">
+                    <img class="ui medium rounded image" src="img/SPORTS2.jpg">
+                    <img class="ui medium rounded image" src="img/SPORTS3.jpg">
+                    <img class="ui medium rounded image" src="img/SPORTS4.jpg">
+                </div>
+
+                <h2 class="ui block header">EID CELEBRATION</h2>
+                <div class="ui medium images">
+                    <img class="ui medium rounded image" src="img/RAYA4.jpg">
+                    <img class="ui medium rounded image" src="img/RAYA1.jpg">
+                    <img class="ui medium rounded image" src="img/RAYA2.jpg">
+                    <img class="ui medium rounded image" src="img/RAYA3.jpg">
+                </div>
+                
+                <h2 class="ui block header">MAULIDUR RASUL CELEBRATION</h2>
+                <div class="ui medium images">
+                    <img class="ui medium rounded image" src="img/RASUL1.jpg">
+                    <img class="ui medium rounded image" src="img/RASUL2.jpg">
+                    <img class="ui medium rounded image" src="img/RASUL3.jpg">
+                    <img class="ui medium rounded image" src="img/RASUL4.jpg">
+                </div>
+                
+                <h2 class="ui block header">COMMUNITY PROGRAM</h2>
+                <div class="ui medium images">
+                    <img class="ui medium rounded image" src="img/COMMUNITY1.jpg">
+                    <img class="ui medium rounded image" src="img/COMMUNITY2.jpg">
+                    <img class="ui medium rounded image" src="img/COMMUNITY3.jpg">
+                    <img class="ui medium rounded image" src="img/COMMUNITY4.jpg">
+                </div>
+
+                <span class="p-20"></span>
+            </div>
+        </div>
+
+    </div>
+    
+<?php include './components/footer.php'; ?>
